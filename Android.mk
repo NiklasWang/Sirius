7a6253b69c02672857f2cf037db7a00bd77d6490
Niklas Wang <niklas.wang@outlook.com>
2018-07-16 17:32:28 +0800

    180716: Implement make rules on Android
    Start to resolve compile issues from next patch.
    
    Rearch Sirius system, Patch 17
    
    There will be several patches to rearch Sirius system,
    Sirius can't be compiled and work before all done.

--- Android.mk
+++ Android.mk
@@ -6,40 +6,49 @@ ENABLE_ANDROID_JNI := no
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-    modules.cpp \
-    Sirius.cpp \
-    SiriusCore.cpp \
-    SiriusImpl.cpp \
-    socket_server.cpp \
-    socket_client.cpp \
-    server_client_common.cpp \
-    SocketClientStateMachine.cpp \
-    SocketServerStateMachine.cpp \
-    SiriusClientCore.cpp \
-    ServerClientControl.cpp \
-    RequestHandler.cpp \
-    UserBufferMgr.cpp \
-    ServerCallbackThread.cpp \
-    IonBufferMgr.cpp \
-    RunOnceThread.cpp \
-    log_impl.cpp \
-    YuvPictureRequestClient.cpp \
-    YuvPictureRequestServer.cpp \
-    PreviewRequestServer.cpp \
-    PreviewRequestClient.cpp \
-    EventRequestServer.cpp \
-    EventRequestClient.cpp \
-    utils/Semaphore.cpp \
-    utils/CQueue.cpp \
-    utils/SyncType.cpp \
-    threadpool/WorkerThread.cpp \
-    threadpool/QueuedThread.cpp \
-    threadpool/ThreadPool.cpp
+LOCAL_SRC_FILES :=                      \
+    core/Sirius.cpp                     \
+    core/SiriusImpl.cpp                 \
+    core/SiriusCore.cpp                 \
+	core/SiriusClientCore.cpp           \
+	core/RequestHandler.cpp             \
+	core/RequestHandlerClient.cpp       \
+	core/ServerCallbackThread.cpp       \
+	core/ServerClientControl.cpp        \
+    socket/socket_server.cpp            \
+    socket/socket_client.cpp            \
+    socket/server_client_common.cpp     \
+    socket/SocketClientStateMachine.cpp \
+    socket/SocketServerStateMachine.cpp \
+	buffer/BufferMgr.cpp                \
+	buffer/IonBufferMgr.cpp             \
+	usecase/EventClient.cpp             \
+	usecase/EventServer.cpp             \
+	usecase/PreviewClient.cpp           \
+	usecase/PreviewServer.cpp           \
+	usecase/YuvPictureClient.cpp        \
+	usecase/YuvPictureServer.cpp        \
+	threads/Thread.cpp                  \
+	threads/ThreadPool.cpp              \
+	threads/ThreadPoolEx.cpp            \
+	threads/ThreadRunner.cpp            \
+	memory/MemMgmt.cpp                  \
+	memory/PoolImpl.cpp                 \
+	log/LogImpl.cpp                     \
+    utils/modules.cpp                   \
+    utils/Semaphore.cpp                 \
+    utils/CQueue.cpp                    \
+    utils/SyncType.cpp
 
-LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/utils \
-    $(LOCAL_PATH)/threadpool
+LOCAL_C_INCLUDES :=       \
+    $(LOCAL_PATH)/utils   \
+    $(LOCAL_PATH)/log     \
+    $(LOCAL_PATH)/core    \
+    $(LOCAL_PATH)/threads \
+    $(LOCAL_PATH)/memory  \
+    $(LOCAL_PATH)/socket  \
+    $(LOCAL_PATH)/buffer  \
+    $(LOCAL_PATH)/usecase
 
 LOCAL_CFLAGS := -std=c++11 -Wall -Wextra -Werror
 LOCAL_CFLAGS += -DENABLE_LOGGER -DBUILD_ANDROID_AP -DMEMORY_DEBUG_MODE
@@ -50,9 +59,11 @@ LOCAL_STATIC_LIBRARIES +=
 
 ifeq ($(ENABLE_ION_MEM),yes)
   LOCAL_CFLAGS           += ENABLE_ION_BUFFER
-  LOCAL_C_INCLUDES       += $(LOCAL_PATH)/android/ion
+  LOCAL_C_INCLUDES       +=
+      $(LOCAL_PATH)/android     \
+      $(LOCAL_PATH)/android/ion \
+      $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
   LOCAL_STATIC_LIBRARIES += libsirius_ion
-  LOCAL_C_INCLUDES       += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
   LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 endif
 
--- tester/Android.mk
+++ tester/Android.mk
@@ -3,12 +3,12 @@ SIRIUS_PATH := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES   := ion/ion.cpp
+LOCAL_SRC_FILES   := Main.cpp ServerTester.cpp
 LOCAL_C_INCLUDES  := $(SIRIUS_PATH)
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SHARED_LIBRARIES := libsirius
 LOCAL_CFLAGS      := -Werror
 LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
-LOCAL_MODULE      := libsirius_ion
+LOCAL_MODULE      := libsirius_server
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_EXECUTABLE)
